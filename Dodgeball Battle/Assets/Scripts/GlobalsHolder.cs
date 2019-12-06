7157019f584abfb2ef81c333a567bf0b0f281a78
Edward Hsu <edhsu@ucsc.edu>
2019-12-05 21:27:10 -0800

    More shenanigans

--- Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
+++ Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 
 public class GlobalsHolder : MonoBehaviour{
     public static int kidsToWin;
+    public static Vector3 rotationPlayer1 = new Vector3(90,0,0);
+    public static Vector3 spawnPointPlayer1 = new Vector3(-50,0,0);
+    public static Vector3 rotationPlayer2 = new Vector3(-90,0,0);
+    public static Vector3 spawnPointPlayer2 = new Vector3(50,0,0);
+
 }
---
+++ Dodgeball Battle/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public List<int> kidStocks = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        kidStocks[0] = 5;
+        kidStocks[1] = 3;
+        kidStocks[2] = 7;
+        kidStocks[3] = 1;
+        kidStocks[4] = 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
