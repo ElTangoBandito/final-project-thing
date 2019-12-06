5bc1aff0e58a223488bcf9f894e5a48856129987
Jason Tsai <jasonexe5@gmail.com>
2019-12-05 20:40:04 -0800

    feat: Add child prefab & script

---
+++ Dodgeball Battle/Assets/ChildController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildController : MonoBehaviour
+{
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool checkDead(){
+        return this.isDead;
+    }
+}
