6af890bccfadf7b96bdb99e8be28f96aea5140f0
Jason Tsai <jasonexe5@gmail.com>
2019-12-05 22:15:06 -0800

    feat: Add child variables

--- Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
+++ Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalsHolder : MonoBehaviour{
-    public static int kidsToWin;
-    public static Vector3 rotationPlayer1 = new Vector3(90,0,0);
-    public static Vector3 spawnPointPlayer1 = new Vector3(-50,0,0);
-    public static Vector3 rotationPlayer2 = new Vector3(-90,0,0);
-    public static Vector3 spawnPointPlayer2 = new Vector3(50,0,0);
 
+[System.Serializable]
+public class ChildInfo
+{
+  public string type;
+  public int moveSpeedUnit;
+  public Vector3 attackRangeUnit;
+  public int attackSpeedUnit;
+}
+public class GlobalsHolder : MonoBehaviour
+{
+  public int kidsToWin;
+  public static Vector3 rotationPlayer1 = new Vector3(90, 0, 0);
+  public static Vector3 spawnPointPlayer1 = new Vector3(-50, 0, 0);
+  public static Vector3 rotationPlayer2 = new Vector3(-90, 0, 0);
+  public static Vector3 spawnPointPlayer2 = new Vector3(50, 0, 0);
+
+
+  public ChildInfo[] childTypes;
 }
