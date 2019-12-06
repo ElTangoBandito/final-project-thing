18e1e98869688d94e87ffdcdce9296a9c849dbf0
Edward Hsu <edhsu@ucsc.edu>
2019-12-05 22:13:38 -0800

    Shenanigans

--- Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
+++ Dodgeball Battle/Assets/Scripts/GlobalsHolder.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GlobalsHolder : MonoBehaviour{
     public static int kidsToWin;
-    public static Vector3 rotationPlayer1 = new Vector3(90,0,0);
-    public static Vector3 spawnPointPlayer1 = new Vector3(-50,0,0);
-    public static Vector3 rotationPlayer2 = new Vector3(-90,0,0);
-    public static Vector3 spawnPointPlayer2 = new Vector3(50,0,0);
+    public static int zSpawnOffset = 2;
+    public static Vector3 rotationPlayer1 = new Vector3(0,90,0);
+    public static Vector3 spawnPointPlayer1 = new Vector3(-22,0,0);
+    public static Vector3 rotationPlayer2 = new Vector3(0,-90,0);
+    public static Vector3 spawnPointPlayer2 = new Vector3(22,0,0);
 
 }
