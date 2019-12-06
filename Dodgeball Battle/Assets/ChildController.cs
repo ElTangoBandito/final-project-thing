3f4554d153a077a6e4e9de651da8e205cda19d86
Jason Tsai <jasonexe5@gmail.com>
2019-12-05 22:22:14 -0800

    feat: Add init for child

--- Dodgeball Battle/Assets/ChildController.cs
+++ Dodgeball Battle/Assets/ChildController.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 
 public class ChildController : MonoBehaviour
 {
-    private bool isDead;
-    private int playerGroup;  //1: P1, 2: P2
-    private int childType;  //0: Normal, 1: Sniper, 2: Catcher, 3: Diagonal, 4: Charger
-    private int laneNum;
+  private bool isDead;
+  private int playerGroup;  //1: P1, 2: P2
+  private int childType;  //0: Normal, 1: Sniper, 2: Catcher, 3: Diagonal, 4: Charger
+  private int laneNum;
 
-    private float movementSpeed;
+  private float movementSpeed;
 
-    // Start is called before the first frame update
-    void Start(int playerGroup, int childType, int laneNum)
+  public void init(int playerGroup, int childType, int laneNum)
+  {
+
+    this.isDead = false;
+    this.playerGroup = playerGroup;
+    this.childType = childType;
+    this.laneNum = laneNum;
+
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (this.childType == 2)
     {
-        this.isDead = false;
-        this.playerGroup = playerGroup;
-        this.childType = childType;
-        this.laneNum = laneNum;
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    else
     {
-        if(this.childType==2){
 
-        }else{
-
-        }
     }
+  }
 
-    public bool checkDead(){
-        return this.isDead;
-    }
+  public bool checkDead()
+  {
+    return this.isDead;
+  }
 }
