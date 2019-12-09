c2d1614c2f89ab1522a51c7719646cb570c90bef
Edward Hsu <edhsu@ucsc.edu>
2019-12-09 15:25:55 -0800

    Integrated final scene with combat scene

--- Dodgeball Battle/Assets/Scripts/CombatSceneController.cs
+++ Dodgeball Battle/Assets/Scripts/CombatSceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class CombatSceneController : MonoBehaviour
@@ -18,80 +19,110 @@ public class CombatSceneController : MonoBehaviour
     private bool player1PieceSelected = false;
     private int player1SelectedPiece = 0;
     private int player1SelectedLane = 0;
-    private List<GameObject> player1CollisionBoxes= new List<GameObject>();
+    private List<GameObject> player1CollisionBoxes = new List<GameObject>();
     private bool player2PieceSelected = false;
     private int player2SelectedPiece = 0;
     private int player2SelectedLane = 0;
-    private List<GameObject> player2CollisionBoxes= new List<GameObject>();
+    private List<GameObject> player2CollisionBoxes = new List<GameObject>();
 
     private bool gameOver = false;
     private string gameOverMessage = "";
 
-    public static void player1GoalReached(){
+    public static void player1GoalReached()
+    {
         player1GoalReachedNumber++;
         GameObject myCanvas = GameObject.Find("Canvas");
         myCanvas.transform.Find("Player1Goal").gameObject.GetComponent<Text>().text = player1GoalReachedNumber.ToString();
     }
-    public static void player2GoalReached(){
+    public static void player2GoalReached()
+    {
         player2GoalReachedNumber++;
         GameObject myCanvas = GameObject.Find("Canvas");
         myCanvas.transform.Find("Player2Goal").gameObject.GetComponent<Text>().text = player2GoalReachedNumber.ToString();
     }
 
-    void checkWinner(){
-        if (player1GoalReachedNumber == kidsToWin && player2GoalReachedNumber == kidsToWin){
+    void checkWinner()
+    {
+        int winner = 0;
+        if (player1GoalReachedNumber == kidsToWin && player2GoalReachedNumber == kidsToWin)
+        {
             gameOverMessage = "Its a tie.";
-        } else if(player1GoalReachedNumber == kidsToWin){
+        }
+        else if (player1GoalReachedNumber == kidsToWin)
+        {
             gameOverMessage = "Player 1 has won";
-        } else if(player2GoalReachedNumber == kidsToWin){
+            winner = 1;
+        }
+        else if (player2GoalReachedNumber == kidsToWin)
+        {
             gameOverMessage = "Player 2 has won";
+            winner = 2;
         }
         bool player1StockIsEmpty = true;
         bool player2StockIsEmpty = true;
-        for (int i = 0; i < Player1Controller.kidStocks.Count; i++){
-            if (Player1Controller.kidStocks[i] > 0){
+        for (int i = 0; i < Player1Controller.kidStocks.Count; i++)
+        {
+            if (Player1Controller.kidStocks[i] > 0)
+            {
                 player1StockIsEmpty = false;
             }
         }
-        for (int i = 0; i < Player2Controller.kidStocks.Count; i++){
-            if (Player2Controller.kidStocks[i] > 0){
+        for (int i = 0; i < Player2Controller.kidStocks.Count; i++)
+        {
+            if (Player2Controller.kidStocks[i] > 0)
+            {
                 player2StockIsEmpty = false;
             }
         }
         bool sceneHasKid = false;
         GameObject[] gameObjectArray = (GameObject[])FindObjectsOfType(typeof(GameObject));
-        for (int i = 0; i < gameObjectArray.Length; i ++){
-            if (gameObjectArray[i].name.Contains("kid")){
+        for (int i = 0; i < gameObjectArray.Length; i++)
+        {
+            if (gameObjectArray[i].name.Contains("kid"))
+            {
                 sceneHasKid = true;
             }
         }
-        if (player1StockIsEmpty && player2StockIsEmpty && !sceneHasKid){
-            if (player1GoalReachedNumber == player2GoalReachedNumber){
+        if (player1StockIsEmpty && player2StockIsEmpty && !sceneHasKid)
+        {
+            if (player1GoalReachedNumber == player2GoalReachedNumber)
+            {
                 gameOverMessage = "Its a tie.";
-            } else if (player1GoalReachedNumber > player2GoalReachedNumber){
+            }
+            else if (player1GoalReachedNumber > player2GoalReachedNumber)
+            {
                 gameOverMessage = "Player 1 has won";
-            } else if (player2GoalReachedNumber > player1GoalReachedNumber){
+                winner = 1;
+            }
+            else if (player2GoalReachedNumber > player1GoalReachedNumber)
+            {
                 gameOverMessage = "Player 2 has won";
+                winner = 2;
             }
         }
-        if (!(string.Equals(gameOverMessage, "")) && !gameOver){
+        if (!(string.Equals(gameOverMessage, "")) && !gameOver)
+        {
             gameOver = true;
-            print(gameOverMessage);
+            GiveMeWinner.winner = winner;
+            //print(gameOverMessage);
         }
-        
+
     }
-    void resetPlayer1(){
+    void resetPlayer1()
+    {
         player1SelectedLane = 0;
         player1SelectedPiece = 0;
         player1PieceSelected = false;
     }
 
-    void resetPlayer2(){
+    void resetPlayer2()
+    {
         player2SelectedLane = 0;
         player2SelectedPiece = 0;
         player2PieceSelected = false;
     }
-    void updateKidsStockUI(){
+    void updateKidsStockUI()
+    {
         GameObject myCanvas = GameObject.Find("Canvas");
         myCanvas.transform.Find("Kid1").gameObject.GetComponent<Text>().text = Player1Controller.kidStocks[0].ToString();
         myCanvas.transform.Find("Kid2").gameObject.GetComponent<Text>().text = Player1Controller.kidStocks[1].ToString();
@@ -107,8 +138,10 @@ public class CombatSceneController : MonoBehaviour
     }
     // Start is called before the first frame update
 
-    void spawnCollisionBoxesPlayer1(){
-        for(int i = 0; i < 4; i++){
+    void spawnCollisionBoxesPlayer1()
+    {
+        for (int i = 0; i < 4; i++)
+        {
             GameObject collisionBox = Instantiate(Resources.Load("Prefabs/CollisionBox", typeof(GameObject)) as GameObject);
             Vector3 boxPos = GlobalsHolder.spawnPointPlayer1;
             boxPos.z += GlobalsHolder.zSpawnOffset * i;
@@ -118,15 +151,17 @@ public class CombatSceneController : MonoBehaviour
         }
     }
 
-    void spawnCollisionBoxesPlayer2(){
-    for(int i = 0; i < 4; i++){
-        GameObject collisionBox = Instantiate(Resources.Load("Prefabs/CollisionBox", typeof(GameObject)) as GameObject);
-        Vector3 boxPos = GlobalsHolder.spawnPointPlayer2;
-        boxPos.z += GlobalsHolder.zSpawnOffset * i;
-        collisionBox.transform.position = boxPos;
-        collisionBox.GetComponent<BoxCollisionScript>().setGroup(2);
-        player2CollisionBoxes.Add(collisionBox);
-    }
+    void spawnCollisionBoxesPlayer2()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject collisionBox = Instantiate(Resources.Load("Prefabs/CollisionBox", typeof(GameObject)) as GameObject);
+            Vector3 boxPos = GlobalsHolder.spawnPointPlayer2;
+            boxPos.z += GlobalsHolder.zSpawnOffset * i;
+            collisionBox.transform.position = boxPos;
+            collisionBox.GetComponent<BoxCollisionScript>().setGroup(2);
+            player2CollisionBoxes.Add(collisionBox);
+        }
     }
 
     void Start()
@@ -137,39 +172,51 @@ public class CombatSceneController : MonoBehaviour
         //Instantiate(kid, transform.position, transform.rotation);
     }
 
-    void getPlayer1Input(){
-        if(player1PieceSelected){
-            if(Input.GetKeyDown("1")){
+    void getPlayer1Input()
+    {
+        if (player1PieceSelected)
+        {
+            if (Input.GetKeyDown("1"))
+            {
                 player1SelectedLane = 1;
-            } 
-            else if (Input.GetKeyDown("2")){
+            }
+            else if (Input.GetKeyDown("2"))
+            {
                 player1SelectedLane = 2;
-            } 
-            else if (Input.GetKeyDown("3")){
+            }
+            else if (Input.GetKeyDown("3"))
+            {
                 player1SelectedLane = 3;
-            } 
-            else if (Input.GetKeyDown("4")){
+            }
+            else if (Input.GetKeyDown("4"))
+            {
                 player1SelectedLane = 4;
-            } 
-            else if (Input.GetKeyDown("0")){
+            }
+            else if (Input.GetKeyDown("0"))
+            {
                 player1SelectedLane = 0;
                 resetPlayer1();
                 //print("Canceling Placement");
             }
-            if(player1SelectedLane > 0){
+            if (player1SelectedLane > 0)
+            {
                 float zPosition = (player1SelectedLane - 1) * GlobalsHolder.zSpawnOffset;
                 Vector3 spawnPos = GlobalsHolder.spawnPointPlayer1;
                 spawnPos.z += zPosition;
 
                 //check if space is occupied
                 bool isSpawnTaken = false;
-                if(player1CollisionBoxes[player1SelectedLane - 1].GetComponent<BoxCollisionScript>().isSpawnBlocked()){
+                if (player1CollisionBoxes[player1SelectedLane - 1].GetComponent<BoxCollisionScript>().isSpawnBlocked())
+                {
                     isSpawnTaken = true;
                 }
 
-                if(isSpawnTaken){
+                if (isSpawnTaken)
+                {
                     //print("Spawn is occupied. Unable to spawn kid.");
-                } else{
+                }
+                else
+                {
                     //print("Placing kid " + player1SelectedPiece + " on lane " + player1SelectedLane);
                     string kidPrefabName = "Prefabs/" + GlobalsHolder.kidPrefabsNameList[player1SelectedPiece - 1];
 
@@ -182,66 +229,89 @@ public class CombatSceneController : MonoBehaviour
                     instance.GetComponent<KidController>().init(1, player1SelectedPiece - 1, player1SelectedLane);
                     Player1Controller.kidStocks[player1SelectedPiece - 1]--;
                 }
-                
+
             }
         }
 
-        if (!player1PieceSelected){
-            if(Input.GetKeyDown("1")){
+        if (!player1PieceSelected)
+        {
+            if (Input.GetKeyDown("1"))
+            {
                 player1SelectedPiece = 1;
                 print("Select kid 1");
-            } else if (Input.GetKeyDown("2")){
+            }
+            else if (Input.GetKeyDown("2"))
+            {
                 player1SelectedPiece = 2;
                 print("Select kid 2");
-            } else if (Input.GetKeyDown("3")){
+            }
+            else if (Input.GetKeyDown("3"))
+            {
                 player1SelectedPiece = 3;
                 print("Select kid 3");
-            } else if (Input.GetKeyDown("4")){
+            }
+            else if (Input.GetKeyDown("4"))
+            {
                 player1SelectedPiece = 4;
                 print("Select kid 4");
-            } else if (Input.GetKeyDown("5")){
+            }
+            else if (Input.GetKeyDown("5"))
+            {
                 player1SelectedPiece = 5;
                 print("Select kid 5");
             }
-            if(player1SelectedPiece != 0 && Player1Controller.kidStocks[player1SelectedPiece - 1] == 0){
+            if (player1SelectedPiece != 0 && Player1Controller.kidStocks[player1SelectedPiece - 1] == 0)
+            {
                 print("Kid" + player1SelectedPiece + " is out of stock.");
                 player1SelectedPiece = 0;
             }
         }
     }
 
-    void getPlayer2Input(){
-        if(player2PieceSelected){
-            if(Input.GetKeyDown("[1]")){
+    void getPlayer2Input()
+    {
+        if (player2PieceSelected)
+        {
+            if (Input.GetKeyDown("[1]"))
+            {
                 player2SelectedLane = 1;
-            } 
-            else if (Input.GetKeyDown("[2]")){
+            }
+            else if (Input.GetKeyDown("[2]"))
+            {
                 player2SelectedLane = 2;
-            } 
-            else if (Input.GetKeyDown("[3]")){
+            }
+            else if (Input.GetKeyDown("[3]"))
+            {
                 player2SelectedLane = 3;
-            } 
-            else if (Input.GetKeyDown("[4]")){
+            }
+            else if (Input.GetKeyDown("[4]"))
+            {
                 player2SelectedLane = 4;
-            } 
-            else if (Input.GetKeyDown("[0]")){
+            }
+            else if (Input.GetKeyDown("[0]"))
+            {
                 player2SelectedLane = 0;
                 resetPlayer2();
                 //print("Canceling Placement");
             }
-            if(player2SelectedLane > 0){
+            if (player2SelectedLane > 0)
+            {
                 float zPosition = (player2SelectedLane - 1) * GlobalsHolder.zSpawnOffset;
                 Vector3 spawnPos = GlobalsHolder.spawnPointPlayer2;
                 spawnPos.z += zPosition;
 
                 //check if space is occupied
                 bool isSpawnTaken = false;
-                if(player2CollisionBoxes[player2SelectedLane - 1].GetComponent<BoxCollisionScript>().isSpawnBlocked()){
+                if (player2CollisionBoxes[player2SelectedLane - 1].GetComponent<BoxCollisionScript>().isSpawnBlocked())
+                {
                     isSpawnTaken = true;
                 }
-                if(isSpawnTaken){
+                if (isSpawnTaken)
+                {
                     //print("Spawn is occupied. Unable to spawn kid.");
-                } else{
+                }
+                else
+                {
                     //print("Placing kid " + player2SelectedPiece + " on lane " + player2SelectedLane);
                     string kidPrefabName = "Prefabs/" + GlobalsHolder.kidPrefabsNameList[player2SelectedPiece - 1];
 
@@ -254,28 +324,39 @@ public class CombatSceneController : MonoBehaviour
                     instance.GetComponent<KidController>().init(2, player2SelectedPiece - 1, player2SelectedLane);
                     Player2Controller.kidStocks[player2SelectedPiece - 1]--;
                 }
-                
+
             }
         }
 
-        if (!player2PieceSelected){
-            if(Input.GetKeyDown("[1]")){
+        if (!player2PieceSelected)
+        {
+            if (Input.GetKeyDown("[1]"))
+            {
                 player2SelectedPiece = 1;
                 print("Select kid 1");
-            } else if (Input.GetKeyDown("[2]")){
+            }
+            else if (Input.GetKeyDown("[2]"))
+            {
                 player2SelectedPiece = 2;
                 print("Select kid 2");
-            } else if (Input.GetKeyDown("[3]")){
+            }
+            else if (Input.GetKeyDown("[3]"))
+            {
                 player2SelectedPiece = 3;
                 print("Select kid 3");
-            } else if (Input.GetKeyDown("[4]")){
+            }
+            else if (Input.GetKeyDown("[4]"))
+            {
                 player2SelectedPiece = 4;
                 print("Select kid 4");
-            } else if (Input.GetKeyDown("[5]")){
+            }
+            else if (Input.GetKeyDown("[5]"))
+            {
                 player2SelectedPiece = 5;
                 print("Select kid 5");
             }
-            if(player2SelectedPiece != 0 && Player2Controller.kidStocks[player2SelectedPiece - 1] == 0){
+            if (player2SelectedPiece != 0 && Player2Controller.kidStocks[player2SelectedPiece - 1] == 0)
+            {
                 print("Kid" + player2SelectedPiece + " is out of stock.");
                 player2SelectedPiece = 0;
             }
@@ -285,28 +366,41 @@ public class CombatSceneController : MonoBehaviour
     void Update()
     {
         //placeKidInPlane
-        getPlayer1Input();
-        getPlayer2Input();
-        if(player1SelectedLane > 0){
-            resetPlayer1();
-        }
+        if (!gameOver)
+        {
+            getPlayer1Input();
+            getPlayer2Input();
+            if (player1SelectedLane > 0)
+            {
+                resetPlayer1();
+            }
 
-        if(player1SelectedPiece > 0){
-            player1PieceSelected = true;
+            if (player1SelectedPiece > 0)
+            {
+                player1PieceSelected = true;
+            }
+            else
+            {
+                resetPlayer1();
+            }
+
+            if (player2SelectedLane > 0)
+            {
+                resetPlayer2();
+            }
+
+            if (player2SelectedPiece > 0)
+            {
+                player2PieceSelected = true;
+            }
+            else
+            {
+                resetPlayer2();
+            }
+            updateKidsStockUI();
+            checkWinner();
         } else{
-            resetPlayer1();
+            SceneManager.LoadScene(3);
         }
-
-        if(player2SelectedLane > 0){
-            resetPlayer2();
-        }
-
-        if(player2SelectedPiece > 0){
-            player2PieceSelected = true;
-        } else{
-            resetPlayer2();
-        }
-        updateKidsStockUI();
-        checkWinner();
     }
 }
---
+++ Dodgeball Battle/Assets/Scripts/GetWinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetWinner : MonoBehaviour
+{
+    public GameObject winnerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winnerText.GetComponent<Text>().text = GiveMeWinner.winner.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
